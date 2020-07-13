3eae972967af06c04ac8d1db7e0b82a2822da3f8
Raffy Traas <66647242+rtraas@users.noreply.github.com>
2020-07-13 16:37:00 -0500

    include "OFF" targets

--- L_BandFDoppSearch.sh
+++ L_BandFDoppSearch.sh
@@ -3,7 +3,7 @@
 ### Modified bash script from /datax/scratch/karenp/turbo_event at the Breakthrough Listen Data Center at UC-Berkeley
 
 for file in /tess_container/seti_tess/l_band/*0000.h5; do
-  if [[ "$file" == *"TIC"* ]]; then
+  if [[ "$file" == *"TIC"* ]] || [[ $"file" == *"HIP"*]]; then
     echo "$file"
   
    time turboSETI "$file" -s 10 -M 4 -o /tess_container/outs/ 
